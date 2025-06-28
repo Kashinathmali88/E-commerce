171cf1ae12372cdb6a3fc209df25febe4c68a7aa
kashinath mali <kashinathmali88@gmail.com>
2025-06-28 13:12:18 +0530

    added api for "/" route to test the api

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,9 @@ app.use("/api/user/", userRouter);
 app.use("/api/product/", productRouter);
 app.use("/api/cart/", cartRouter);
 app.use("/api/order", orderRouter);
+app.get("/", (req, res) => {
+  res.send("Api working!!!");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
